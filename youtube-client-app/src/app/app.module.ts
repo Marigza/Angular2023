6cdd86ebfae7378ae735e87c0c88ba2d5ac0bc22
Marigza <marigola@mail.ru>
2023-10-25 17:15:34 +0300

    feat: add angular material

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { SearchResultsComponent } from './search-module/search-results/search-re
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
