b9da757e0380cee8347544fc62dcf4c03a4b5417
Marigza <marigola@mail.ru>
2023-10-26 14:17:34 +0300

    feat: add search service

--- youtube-client-app/src/app/search-module/search-results/search-results.component.ts
+++ youtube-client-app/src/app/search-module/search-results/search-results.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+import { SearchItem } from '../search-item.model';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'yta-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent {}
+export class SearchResultsComponent implements OnChanges {
+  public searchResult: SearchItem[] = [];
+
+  @Input() public isShown = false;
+
+  constructor(private searchService: SearchService) {}
+
+  public ngOnChanges(): void {
+    if (this.isShown) this.showItems();
+  }
+
+  public showItems(): void {
+    this.searchResult = this.searchService.getResult();
+  }
+}
