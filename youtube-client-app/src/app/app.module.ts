ea6c1cffdd486ca0d7829631ffe4cf5c4146d86e
Marigza <marigola@mail.ru>
2023-10-29 16:25:00 +0300

    feat: change app module

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,17 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { SearchItemComponent } from './search-module/search-item/search-item.component';
+import { CoreModule } from './core/core.module';
+import { ItemComponent } from './search-module/item/item.component';
 import { SearchResultsComponent } from './search-module/search-results/search-results.component';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { ColoringDirective } from './shared/directives/coloring.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchResultsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SearchItemComponent, ButtonComponent],
+  declarations: [AppComponent, SearchResultsComponent, ColoringDirective],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ItemComponent,
+    ButtonComponent,
+    HttpClientModule,
+    CoreModule,
+    CommonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
