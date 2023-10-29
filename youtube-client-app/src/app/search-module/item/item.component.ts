e970c9e66fa91682a4f9b8423d7c6860bff5a926
Marigza <marigola@mail.ru>
2023-10-29 16:22:29 +0300

    refactor: rename component

---
+++ youtube-client-app/src/app/search-module/item/item.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { SearchItem } from '../../shared/models/search-item.model';
+
+@Component({
+  selector: 'yta-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatIconModule, ButtonComponent],
+})
+export class ItemComponent {
+  @Input() public card!: SearchItem;
+}
