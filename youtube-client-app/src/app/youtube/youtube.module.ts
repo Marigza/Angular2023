3ddb62e1faa1b4dae2600402c03465820b8650a4
Marigza <marigola@mail.ru>
2023-11-04 16:30:26 +0300

    refactor: change structure

--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ButtonComponent } from '../shared/components/button/button.component';
 import { ItemComponent } from './components/item/item.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { ColoringDirective } from './directives/coloring.directive';
+import { MaterialModule } from './material.module';
+import { DetailedInfoPageComponent } from './pages/detailed-info-page/detailed-info-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 import { YoutubeRoutingModule } from './youtube-routing.module';
 
 @NgModule({
-  declarations: [MainPageComponent, SearchResultsComponent],
-  imports: [CommonModule, ItemComponent, YoutubeRoutingModule],
+  declarations: [MainPageComponent, SearchResultsComponent, ItemComponent, DetailedInfoPageComponent],
+  imports: [CommonModule, ButtonComponent, MaterialModule, YoutubeRoutingModule, ColoringDirective],
 })
 export class YoutubeModule {}
