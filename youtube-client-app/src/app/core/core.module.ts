4a3f0a851790b6aa26d49d37b7e7805f1d11fbce
Marigza <marigola@mail.ru>
2023-11-04 11:09:06 +0300

    feat: add page-not-found component to module

--- youtube-client-app/src/app/core/core.module.ts
+++ youtube-client-app/src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { ButtonComponent } from '../shared/components/button/button.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { FilterBlockComponent } from './filter-block/filter-block.component';
 import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FilterBlockComponent],
+  declarations: [HeaderComponent, FilterBlockComponent, PageNotFoundComponent],
   imports: [CommonModule, CoreRoutingModule, ButtonComponent],
   exports: [HeaderComponent],
 })
