c13640ab9b93cc3cf87e9c33aa971c9839feec9e
Marigza <marigola@mail.ru>
2023-11-04 16:22:20 +0300

    feat: change item component

--- youtube-client-app/src/app/youtube/components/item/item.component.ts
+++ youtube-client-app/src/app/youtube/components/item/item.component.ts
@@ -1,18 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 
-import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { ColoringDirective } from '../../directives/coloring.directive';
 import { SearchItem } from '../../models/search-item.model';
 
 @Component({
   selector: 'yta-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss'],
-  standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule, ButtonComponent, ColoringDirective],
 })
 export class ItemComponent {
   @Input() public card!: SearchItem;
