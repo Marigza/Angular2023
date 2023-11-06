7cd6370f15515fe61e84615cfcd2dda95a83548f
Marigza <marigola@mail.ru>
2023-11-06 18:30:51 +0300

    feat: add auth guard

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { authGuard } from './auth/auth.guard';
+import { authGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
