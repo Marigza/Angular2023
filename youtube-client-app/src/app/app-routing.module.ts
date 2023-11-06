0ce82f30a1d73d1551a6f9625c4c0ec2ca06b9fc
Marigza <marigola@mail.ru>
2023-11-06 18:39:38 +0300

    feat: add login guard

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { authGuard } from './auth/guards/auth.guard';
+import { loginGuard } from './auth/guards/login.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [loginGuard],
   },
   {
     path: 'youtube',
