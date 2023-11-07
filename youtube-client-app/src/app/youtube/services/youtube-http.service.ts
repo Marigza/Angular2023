c865cfa13028102ff91ff7478b5193c73f27084a
Marigza <marigola@mail.ru>
2023-11-07 17:41:00 +0300

    feat: add error handler

--- youtube-client-app/src/app/youtube/services/youtube-http.service.ts
+++ youtube-client-app/src/app/youtube/services/youtube-http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { SearchResponse } from '../models/search-response.model';
 
@@ -11,6 +12,19 @@ export class YoutubeHttpService {
   constructor(private http: HttpClient) {}
 
   public get(): Observable<SearchResponse> {
-    return this.http.get<SearchResponse>('assets/data.json'); // добавить обработку ошибок catchError...of
+    return this.http.get<SearchResponse>('assets/data.json').pipe(catchError(this.handleError<SearchResponse>('get')));
   }
+
+  /* eslint-disable class-methods-use-this, no-alert */
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: Error): Observable<T> => {
+      console.error(error);
+      alert(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  /* eslint-enable class-methods-use-this,  no-alert */
 }
