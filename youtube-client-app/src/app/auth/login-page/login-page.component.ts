a08a799af4c68ea1d0d075510f9ba4fb89df8f63
Marigza <marigola@mail.ru>
2023-11-12 15:37:19 +0300

    feat: add validation to login page

--- youtube-client-app/src/app/auth/login-page/login-page.component.ts
+++ youtube-client-app/src/app/auth/login-page/login-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { passwordValidator } from '../../shared/validators/password.validator';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -10,19 +11,66 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
-  public loginControl = new FormControl('');
+  public isHide = true;
 
-  public passwordControl = new FormControl('');
+  public login = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, passwordValidator]],
+  });
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private formBuilder: FormBuilder
   ) {}
 
   public async onSubmit(): Promise<void> {
-    if (this.loginControl.value && this.passwordControl.value) {
-      this.authService.login(this.loginControl.value);
+
+    if (this.loginValue && this.passValue) {
+      this.authService.login(this.loginValue);
       await this.router.navigate(['/youtube']);
     }
+  }
+
+  public get loginValue(): string | null | undefined {
+    if (this.emailFormField?.valid) {
+      return this.emailFormField?.value;
+    }
+
+    return undefined;
+  }
+
+  public get passValue(): string | null | undefined {
+    if (this.passwordFormField?.valid) {
+      return this.passwordFormField?.value;
+    }
+
+    return undefined;
+  }
+
+  public getErrorEmailMessage(): string {
+    if (this.emailFormField?.hasError('required')) {
+      return 'Please enter a login email';
+    }
+
+    return this.emailFormField?.hasError('email') ? 'The login email is invalid' : '';
+  }
+
+  public getErrorPasswordMessage(): string {
+    if (this.passwordFormField?.hasError('required')) {
+      return 'Please enter a password';
+    }
+
+    return this.passwordFormField?.errors
+      ? `Your password is not strong enough: ${this.passwordFormField?.errors['requiredValue']}`
+      : '';
+  }
+
+  private get passwordFormField(): AbstractControl<string | null, string | null> | null {
+    return this.login.get('password');
+  }
+
+  private get emailFormField(): AbstractControl<string | null, string | null> | null {
+    return this.login.get('email');
   }
 }
