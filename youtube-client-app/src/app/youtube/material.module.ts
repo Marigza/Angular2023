d4005e91425a7e9f954e5b8e4c724462b416dc6c
Marigza <marigola@mail.ru>
2023-11-12 19:15:37 +0300

    feat: update modules

--- youtube-client-app/src/app/youtube/material.module.ts
+++ youtube-client-app/src/app/youtube/material.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatCardModule, MatIconModule],
+  exports: [MatCardModule, MatIconModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../shared/components/button/button.component';
 import { ItemComponent } from './components/item/item.component';
@@ -7,6 +8,7 @@ import { SearchResultsComponent } from './components/search-results/search-resul
 import { StatisticBlockComponent } from './components/statistic-block/statistic-block.component';
 import { ColoringDirective } from './directives/coloring.directive';
 import { MaterialModule } from './material.module';
+import { AdminComponent } from './pages/admin/admin.component';
 import { DetailedInfoPageComponent } from './pages/detailed-info-page/detailed-info-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { YoutubeRoutingModule } from './youtube-routing.module';
@@ -18,7 +20,15 @@ import { YoutubeRoutingModule } from './youtube-routing.module';
     ItemComponent,
     DetailedInfoPageComponent,
     StatisticBlockComponent,
+    AdminComponent,
   ],
-  imports: [CommonModule, ButtonComponent, MaterialModule, YoutubeRoutingModule, ColoringDirective],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonComponent,
+    MaterialModule,
+    YoutubeRoutingModule,
+    ColoringDirective,
+  ],
 })
 export class YoutubeModule {}
