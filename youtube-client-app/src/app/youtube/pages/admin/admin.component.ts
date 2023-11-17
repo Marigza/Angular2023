9c1a857f0098e12cf771fb2e0537d513eb3f1226
Marigza <marigola@mail.ru>
2023-11-17 19:25:18 +0300

    refactor: update validation on admin page

--- youtube-client-app/src/app/youtube/pages/admin/admin.component.ts
+++ youtube-client-app/src/app/youtube/pages/admin/admin.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
-
-import { dateValidator } from '../../../shared/validators/date.validator';
-import { ValidationService } from '../../services/validation.service';
+import { AbstractControl, NonNullableFormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'yta-admin',
@@ -10,61 +7,28 @@ import { ValidationService } from '../../services/validation.service';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
+  public currentDate = new Date();
+
   public createCard = this.formBuilder.group({
     title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
     description: ['', [Validators.maxLength(255)]],
     image: ['', [Validators.required]],
     video: ['', [Validators.required]],
-    date: ['', [Validators.required, Validators.maxLength(10), dateValidator]],
+    date: ['', [Validators.required, Validators.maxLength(10), this.dateValidator()]],
   });
 
-  public arrayFields = [
-    {
-      label: 'title',
-      errorMessage: (): string => this.validationService.getErrorTitleMessage(this.createCard.get('title')),
-      requiered: true,
-      type: 'text',
-    },
-    {
-      label: 'description',
-      errorMessage: (): string => this.validationService.getErrorDescriptionMessage(this.createCard.get('description')),
-      requiered: false,
-      type: 'text',
-    },
-    {
-      label: 'image',
-      errorMessage: (): string => this.validationService.getErrorImageMessage(this.createCard.get('image')),
-      requiered: true,
-      type: 'text',
-    },
-    {
-      label: 'video',
-      errorMessage: (): string => this.validationService.getErrorVideoMessage(this.createCard.get('video')),
-      requiered: true,
-      type: 'text',
-    },
-    {
-      label: 'date',
-      errorMessage: (): string => this.validationService.getErrorDateMessage(this.createCard.get('date')),
-      requiered: true,
-      type: 'date',
-    },
-  ];
+  constructor(private formBuilder: NonNullableFormBuilder) {}
 
-  constructor(
-    private formBuilder: NonNullableFormBuilder,
-    private validationService: ValidationService
-  ) {}
+  public dateValidator(): ValidatorFn {
+    return (control: AbstractControl<string>): ValidationErrors | null => {
+      const inputData = new Date(control.value);
+      const deltaTime = Number(this.currentDate) - Number(inputData);
+
+      return deltaTime < 0 ? { dateInFuture: true } : null;
+    };
+  }
 
   public resetForm(): void {
     this.createCard.reset();
   }
-
-  /* eslint-disable class-methods-use-this */
-
-  public trackByIndex(index: number): number {
-    return index;
-  }
-
-  /* eslint-enable class-methods-use-this */
 }
