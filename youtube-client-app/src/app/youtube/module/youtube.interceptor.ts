71f4fc96e93d0ed5a27930534f07d36aebeb0dda
Marigza <marigola@mail.ru>
2023-11-18 00:10:36 +0300

    feat: environment file

--- youtube-client-app/src/app/youtube/module/youtube.interceptor.ts
+++ youtube-client-app/src/app/youtube/module/youtube.interceptor.ts
@@ -2,14 +2,18 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable()
 export class YoutubeInterceptor implements HttpInterceptor {
-  private url = `https://youtube.googleapis.com/youtube/v3/`;
+  private apiKey = environment.apiKey;
+
+  private apiUrl = environment.urlYoutube;
 
   public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const url = `${this.url}${request.url}`; // from env
+    const url = `${this.apiUrl}${request.url}`;
     const updateRequest = request.clone({
-      setParams: { key: 'AIzaSyDJhkBMN6u7R12Jp1zoGWxjrq0-2xYafK8' }, // from env
+      setParams: { key: this.apiKey },
       url,
     });
 
---
+++ youtube-client-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  urlYoutube: 'https://youtube.googleapis.com/youtube/v3/',
+  apiKey: 'AIzaSyDJhkBMN6u7R12Jp1zoGWxjrq0-2xYafK8',
+};
