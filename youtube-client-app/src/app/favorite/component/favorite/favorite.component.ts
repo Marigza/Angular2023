dfcb34c136a4740b27526491d9b6695c19caf9a4
Marigza <marigola@mail.ru>
2023-11-22 23:51:55 +0300

    feat: add content to favorite component

--- youtube-client-app/src/app/favorite/component/favorite/favorite.component.ts
+++ youtube-client-app/src/app/favorite/component/favorite/favorite.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { selectFavoriteCards } from 'src/app/redux/selectors/custom-cards.selector';
+import { ItemWithDetails } from 'src/app/youtube/models/item-with-details.model';
 
 @Component({
   selector: 'yta-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.scss'],
 })
-export class FavoriteComponent {}
+export class FavoriteComponent {
+  public favoriteCards$: Observable<ItemWithDetails[]> = this.store.select(selectFavoriteCards);
+
+  constructor(private store: Store) {}
+
+  /* eslint-disable class-methods-use-this */
+
+  public trackByCards(index: number, item: ItemWithDetails): string {
+    return item.id;
+  }
+
+  /* eslint-enable class-methods-use-this */
+}
