3f1ff34e5de639c57d0983fa2b85aa151f69b04a
Marigza <marigola@mail.ru>
2023-11-22 23:44:15 +0300

    feat: favorite cards selector

--- youtube-client-app/src/app/redux/selectors/custom-cards.selector.ts
+++ youtube-client-app/src/app/redux/selectors/custom-cards.selector.ts
@@ -6,3 +6,5 @@ import { cardsFeatureKey } from '../reducers/cards.reducer';
 export const selectFeature = createFeatureSelector<YoutubeStore>(cardsFeatureKey);
 
 export const selectCustomCards = createSelector(selectFeature, (state: YoutubeStore) => state.customCards);
+
+export const selectFavoriteCards = createSelector(selectFeature, (state: YoutubeStore) => state.favoriteCards);
