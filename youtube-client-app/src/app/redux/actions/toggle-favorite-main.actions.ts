e71c724f1584844f2efecab43dd2502d937cf37c
Marigza <marigola@mail.ru>
2023-11-22 23:39:30 +0300

    feat: actions for favirite cards

---
+++ youtube-client-app/src/app/redux/actions/toggle-favorite-main.actions.ts
@@ -0,0 +1,11 @@
+import { createActionGroup, props } from '@ngrx/store';
+
+import { ItemWithDetails } from '../../youtube/models/item-with-details.model';
+
+export const mainPageActions = createActionGroup({
+  source: 'Main Page',
+  events: {
+    'Add Favorite Card': props<{ card: ItemWithDetails }>(),
+    'Delete Favorite Card': props<{ id: string }>(),
+  },
+});
---
+++ youtube-client-app/src/app/redux/actions/toggle-favorite-view.actions.ts
@@ -0,0 +1,11 @@
+import { createActionGroup, props } from '@ngrx/store';
+
+import { ItemWithDetails } from '../../youtube/models/item-with-details.model';
+
+export const viewPageActions = createActionGroup({
+  source: 'View Page',
+  events: {
+    'Add Favorite Card': props<{ card: ItemWithDetails }>(),
+    'Delete Favorite Card': props<{ id: string }>(),
+  },
+});
