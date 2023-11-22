2b5459c87dd4a6e6ef0e17c3864ab501c32563f3
Marigza <marigola@mail.ru>
2023-11-22 23:43:10 +0300

    feat: reducer for favorite cards

--- youtube-client-app/src/app/redux/reducers/cards.reducer.ts
+++ youtube-client-app/src/app/redux/reducers/cards.reducer.ts
@@ -2,6 +2,7 @@ import { createReducer, on } from '@ngrx/store';
 
 import { youtubeApiActions } from '../actions/card-api.actions';
 import { adminCardActions } from '../actions/custom-card.actions';
+import { mainPageActions } from '../actions/toggle-favorite-main.actions';
 import { YoutubeStore } from '../models/youtube-store.model';
 
 export const initialState: YoutubeStore = {
@@ -18,5 +19,18 @@ export const customCardReducer = createReducer(
   on(adminCardActions.createCustomCard, (state, card) => ({
     ...state,
     customCards: state.customCards.concat(card.card),
+  })),
+  on(mainPageActions.addFavoriteCard, (state, action) => ({
+    ...state,
+    favoriteCards: state.favoriteCards.concat(action.card),
+  })),
+  on(mainPageActions.deleteFavoriteCard, (state, action) => ({
+    ...state,
+    favoriteCards: state.favoriteCards
+      .slice(
+        0,
+        state.favoriteCards.findIndex(({ id }) => id === action.id)
+      )
+      .concat(state.favoriteCards.slice(state.favoriteCards.findIndex(({ id }) => id === action.id) + 1)),
   }))
 );
