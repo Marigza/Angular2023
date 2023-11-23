f9a78db7ab8b913f35341c16d44eb10603935ad9
Marigza <marigola@mail.ru>
2023-11-23 13:26:20 +0300

    feat: add acitons to reducer

--- youtube-client-app/src/app/redux/reducers/cards.reducer.ts
+++ youtube-client-app/src/app/redux/reducers/cards.reducer.ts
@@ -2,7 +2,8 @@ import { createReducer, on } from '@ngrx/store';
 
 import { youtubeApiActions } from '../actions/card-api.actions';
 import { adminCardActions } from '../actions/custom-card.actions';
-import { mainPageActions } from '../actions/toggle-favorite-main.actions';
+import { mainPageActions } from '../actions/main-page-card.actions';
+import { viewPageActions } from '../actions/view-page-card.actions';
 import { YoutubeStore } from '../models/youtube-store.model';
 
 export const initialState: YoutubeStore = {
@@ -20,11 +21,20 @@ export const customCardReducer = createReducer(
     ...state,
     customCards: state.customCards.concat(card.card),
   })),
-  on(mainPageActions.addFavoriteCard, (state, action) => ({
+  on(mainPageActions.deleteCustomCard, viewPageActions.deleteCustomCard, (state, action) => ({
+    ...state,
+    customCards: state.customCards
+      .slice(
+        0,
+        state.customCards.findIndex(({ id }) => id === action.id)
+      )
+      .concat(state.customCards.slice(state.customCards.findIndex(({ id }) => id === action.id) + 1)),
+  })),
+  on(mainPageActions.addFavoriteCard, viewPageActions.addFavoriteCard, (state, action) => ({
     ...state,
     favoriteCards: state.favoriteCards.concat(action.card),
   })),
-  on(mainPageActions.deleteFavoriteCard, (state, action) => ({
+  on(mainPageActions.deleteFavoriteCard, viewPageActions.deleteFavoriteCard, (state, action) => ({
     ...state,
     favoriteCards: state.favoriteCards
       .slice(
