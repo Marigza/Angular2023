8fb00aebde9c942e23c9c24eff0011cfaee5a097
Marigza <marigola@mail.ru>
2023-11-23 13:28:01 +0300

    feat: add actions, change naming

--- youtube-client-app/src/app/redux/actions/main-page-card.actions.ts
+++ youtube-client-app/src/app/redux/actions/main-page-card.actions.ts
@@ -2,10 +2,11 @@ import { createActionGroup, props } from '@ngrx/store';
 
 import { ItemWithDetails } from '../../youtube/models/item-with-details.model';
 
-export const viewPageActions = createActionGroup({
-  source: 'View Page',
+export const mainPageActions = createActionGroup({
+  source: 'Main Page',
   events: {
     'Add Favorite Card': props<{ card: ItemWithDetails }>(),
     'Delete Favorite Card': props<{ id: string }>(),
+    'Delete Custom Card': props<{ id: string }>(),
   },
 });
--- youtube-client-app/src/app/redux/actions/view-page-card.actions.ts
+++ youtube-client-app/src/app/redux/actions/view-page-card.actions.ts
@@ -2,10 +2,11 @@ import { createActionGroup, props } from '@ngrx/store';
 
 import { ItemWithDetails } from '../../youtube/models/item-with-details.model';
 
-export const mainPageActions = createActionGroup({
-  source: 'Main Page',
+export const viewPageActions = createActionGroup({
+  source: 'View Page',
   events: {
     'Add Favorite Card': props<{ card: ItemWithDetails }>(),
     'Delete Favorite Card': props<{ id: string }>(),
+    'Delete Custom Card': props<{ id: string }>(),
   },
 });
