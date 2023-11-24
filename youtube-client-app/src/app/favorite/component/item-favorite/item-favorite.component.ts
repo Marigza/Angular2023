ba6d8ae97cc4f60e95224fca6a3a7094838046b3
Marigza <marigola@mail.ru>
2023-11-25 01:16:52 +0300

    feat: item favorite component

---
+++ youtube-client-app/src/app/favorite/component/item-favorite/item-favorite.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+import { CardsStoreFacadeService } from '../../../shared/services/cards-store-facade.service';
+import { ItemWithDetails } from '../../../youtube/models/item-with-details.model';
+
+@Component({
+  selector: 'yta-item-favorite',
+  templateUrl: './item-favorite.component.html',
+  styleUrls: ['./item-favorite.component.scss'],
+})
+export class ItemFavoriteComponent {
+  @Input() public card!: ItemWithDetails;
+
+  public isFavorite = true;
+
+  // public isFavorite = this.store.select(selectFavoriteCards).pipe(map(cards => cards.map(({ id }) => id).includes(this.card.id)));
+
+  // card.isFavorite create logic
+
+  constructor(private cardsStoreFacadeService: CardsStoreFacadeService) {}
+
+  public toggleFavorite(): void {
+    this.isFavorite
+      ? this.cardsStoreFacadeService.deleteFavoriteCard(this.card.id)
+      : this.cardsStoreFacadeService.addFavoriteCard(this.card);
+
+    this.isFavorite = !this.isFavorite;
+  }
+
+  public get isCustomCard(): boolean {
+    return this.card?.kind === 'custom#video';
+  }
+
+  public deleteCustomCard(): void {
+    this.cardsStoreFacadeService.deleteCustomCard(this.card.id);
+  }
+}
--- youtube-client-app/src/app/favorite/favorite.module.ts
+++ youtube-client-app/src/app/favorite/favorite.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { ButtonComponent } from '../shared/components/button/button.component';
 import { FavoriteComponent } from './component/favorite/favorite.component';
+import { ItemFavoriteComponent } from './component/item-favorite/item-favorite.component';
 import { FavoriteRoutingModule } from './favorite-routing.module';
 import { MatetialModule } from './material.module';
 
 @NgModule({
-  declarations: [FavoriteComponent],
+  declarations: [FavoriteComponent, ItemFavoriteComponent],
   imports: [CommonModule, MatetialModule, FavoriteRoutingModule, ButtonComponent],
 })
 export class FavoriteModule {}
