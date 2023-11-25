b57c6865cb243e0169ad651915a802f22fac3c2e
Marigza <marigola@mail.ru>
2023-11-25 11:09:59 +0300

    fix: routing

--- youtube-client-app/src/app/favorite/favorite-routing.module.ts
+++ youtube-client-app/src/app/favorite/favorite-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DetailedInfoPageComponent } from '../youtube/pages/detailed-info-page/detailed-info-page.component';
 import { FavoriteComponent } from './component/favorite/favorite.component';
 
-const routes: Routes = [{ path: '', component: FavoriteComponent }];
+const routes: Routes = [
+  {
+    children: [
+      { path: '', component: FavoriteComponent },
+      { path: ':cardId', component: DetailedInfoPageComponent },
+    ],
+    path: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
