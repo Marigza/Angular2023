c002dd70b4e08d02ce4eabc3a41c3aadcc1bebde
Marigza <marigola@mail.ru>
2023-12-05 10:05:17 +0300

    fix: routing

--- connections-app/src/app/auth/auth.module.ts
+++ connections-app/src/app/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SignupComponent, SigninComponent],
+  exports: [SignupComponent, SigninComponent],
   imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
--- connections-app/src/app/shared/shared.module.ts
+++ connections-app/src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PageNotFoundComponent],
+  imports: [CommonModule],
 })
 export class SharedModule {}
