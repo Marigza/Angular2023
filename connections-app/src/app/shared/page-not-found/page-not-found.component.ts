d02831b870e02c17ba6d21d475f84a351bb649c0
Marigza <marigola@mail.ru>
2023-12-09 15:11:44 +0300

    feat: routing module for 404 page

--- connections-app/src/app/shared/page-not-found/page-not-found.component.ts
+++ connections-app/src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'con-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
-  imports: [MatButtonModule],
+  imports: [RouterModule, MatButtonModule],
   standalone: true,
 })
 export class PageNotFoundComponent {}
