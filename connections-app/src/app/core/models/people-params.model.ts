b53001628eef7ddbb9379b57f677768a7afea92f
Marigza <marigola@mail.ru>
2023-12-09 16:16:30 +0300

    feat: models

---
+++ connections-app/src/app/core/models/people-params.model.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+export interface PeopleParams {
+  name: {
+    S: string;
+  };
+  uid: {
+    S: string;
+  };
+}
+
+/* eslint-enable @typescript-eslint/naming-convention */
