39a03a72ec6997b66852236c0ef7f24ef13ecaa2
Marigza <marigola@mail.ru>
2023-12-12 21:28:02 +0300

    feat: groupDeducatedComponent

--- connections-app/src/app/connections/connections.module.ts
+++ connections-app/src/app/connections/connections.module.ts
@@ -4,13 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ConnectionsBlockComponent } from './connections-block/connections-block.component';
 import { ConnectionsRoutingModule } from './connections-routing.module';
+import { GroupDedicatedComponent } from './group-dedicated/group-dedicated.component';
 import { GroupComponent } from './group/group.component';
 import { MaterialModule } from './material.module';
 import { PeopleComponent } from './people/people.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [ConnectionsBlockComponent, GroupComponent, PeopleComponent, ProfileComponent],
+  declarations: [ConnectionsBlockComponent, GroupComponent, PeopleComponent, ProfileComponent, GroupDedicatedComponent],
   imports: [CommonModule, ReactiveFormsModule, ConnectionsRoutingModule, MaterialModule],
 })
 export class ConnectionsModule {}
---
+++ connections-app/src/app/connections/group-dedicated/group-dedicated.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'con-group-dedicated',
+  templateUrl: './group-dedicated.component.html',
+  styleUrls: ['./group-dedicated.component.scss'],
+})
+export class GroupDedicatedComponent {}
