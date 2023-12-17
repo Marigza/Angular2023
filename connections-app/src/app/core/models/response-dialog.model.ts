745ff4d250daff6b51f781515d0a6e399a80fd0b
Marigza <marigola@mail.ru>
2023-12-17 12:30:06 +0300

    feat: models

---
+++ connections-app/src/app/core/models/response-dialog.model.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MessageParams } from './message-params.model';
+
+export interface ResponseDialog {
+  Count: number;
+  Items: MessageParams[];
+}
+
+/* eslint-enable @typescript-eslint/naming-convention */
